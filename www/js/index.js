8528c96aafe810c37e913792022e76c591622b32
shoukathayat222 <shoukathayat222@gmail.com>
2015-01-18 00:47:13 +0500

    camera
    test

--- www/js/index.js
+++ www/js/index.js
@@ -169,7 +169,9 @@ function onPhotoURISuccess(imageURI) {
 function capturePhotoWithData() {
 	alert('capturePhotoWithData');
   // Take picture using device camera and retrieve image as base64-encoded string
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+  //navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
 }
 
 function capturePhotoWithFile() {
