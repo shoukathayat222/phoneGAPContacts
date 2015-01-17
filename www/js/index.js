eb806c724d3de6ebd52b4ee616be01e27772a0a4
shoukathayat222 <shoukathayat222@gmail.com>
2015-01-18 00:30:53 +0500

    camera
    camera test

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,8 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value 
+
 function onDeviceReady() {
     // we will not be doing anything!!
 	pictureSource=navigator.camera.PictureSourceType;
@@ -101,88 +104,85 @@ function resetPage() {
     $("#number").val("");
 }
 
-	var pictureSource;   // picture source
-    var destinationType; // sets the format of returned value 
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+  // Get image handle
+  //
+  var smallImage = document.getElementById('smallImage');
 
-    // Called when a photo is successfully retrieved
-    //
-    function onPhotoDataSuccess(imageData) {
-      // Get image handle
-      //
-      var smallImage = document.getElementById('smallImage');
+  // Unhide image elements
+  //
+  smallImage.style.display = 'block';
 
-      // Unhide image elements
-      //
-      smallImage.style.display = 'block';
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  //
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+}
 
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      //
-      smallImage.src = "data:image/jpeg;base64," + imageData;
-    }
-    
-	// Called when a photo is successfully retrieved
-    //
-    function onPhotoFileSuccess(imageData) {
-      // Get image handle
-      console.log(JSON.stringify(imageData));
-      
-   	  // Get image handle
-      //
-      var smallImage = document.getElementById('smallImage');
+// Called when a photo is successfully retrieved
+//
+function onPhotoFileSuccess(imageData) {
+  // Get image handle
+  console.log(JSON.stringify(imageData));
+  
+  // Get image handle
+  //
+  var smallImage = document.getElementById('smallImage');
 
-      // Unhide image elements
-      //
-      smallImage.style.display = 'block';
+  // Unhide image elements
+  //
+  smallImage.style.display = 'block';
 
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      //
-      smallImage.src = imageData;
-    }
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  //
+  smallImage.src = imageData;
+}
 
-    // Called when a photo is successfully retrieved
-    //
-    function onPhotoURISuccess(imageURI) {
-      // Uncomment to view the image file URI 
-      // console.log(imageURI);
+// Called when a photo is successfully retrieved
+//
+function onPhotoURISuccess(imageURI) {
+  // Uncomment to view the image file URI 
+  // console.log(imageURI);
 
-      // Get image handle
-      //
-      var largeImage = document.getElementById('largeImage');
+  // Get image handle
+  //
+  var largeImage = document.getElementById('largeImage');
 
-      // Unhide image elements
-      //
-      largeImage.style.display = 'block';
+  // Unhide image elements
+  //
+  largeImage.style.display = 'block';
 
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      //
-      largeImage.src = imageURI;
-    }
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  //
+  largeImage.src = imageURI;
+}
 
-    // A button will call this function
-    //
-    function capturePhotoWithData() {
-      // Take picture using device camera and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
-    }
+// A button will call this function
+//
+function capturePhotoWithData() {
+  // Take picture using device camera and retrieve image as base64-encoded string
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+}
 
-    function capturePhotoWithFile() {
-        navigator.camera.getPicture(onPhotoFileSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });
-    }
-    
-    // A button will call this function
-    //
-    function getPhoto(source) {
-      // Retrieve image file location from specified source
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
-        destinationType: destinationType.FILE_URI,
-        sourceType: source });
-    }
+function capturePhotoWithFile() {
+	navigator.camera.getPicture(onPhotoFileSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });
+}
 
-    // Called if something bad happens.
-    // 
-    function onFail(message) {
-      alert('Failed because: ' + message);
-    }
+// A button will call this function
+//
+function getPhoto(source) {
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+	destinationType: destinationType.FILE_URI,
+	sourceType: source });
+}
+
+// Called if something bad happens.
+// 
+function onFail(message) {
+  alert('Failed because: ' + message);
+}
